49efd1a5958c3f53c2f45ebc54e5188507d6f95c
nguyentrananhthu9103@gmail.com <nguyentrananhthu9103@gmail.com>
2024-03-19 09:02:13 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,11 +1,21 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-
-class Form1(Form1Template):
   
-
+class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    # Any code you write here will run before the form opens.
+
+  def btnNHAP_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    arr = list(map(int, input().split(",")))
+    self.label_MANG.text = str(arr)
+    self.btnSX_click(arr)
+    pass
+    
+  def btnSX_click(self, **event_args):
+    """This method is called when the button is clicked"""
     def insertion_sort(arr):
       for i in range(1, len(arr)):
         current_element = arr[i]
@@ -15,7 +25,9 @@ class Form1(Form1Template):
           j -= 1
         arr[j + 1] = current_element
       return arr
-    self.init_components(**properties)
-    arr = list(map(int, input(self.txtN.split(",")))
+  
+    sorted_arr = insertion_sort(arr)
+    self.label_KQ = {sorted_arr}
+    pass
 
-    # Any code you write here will run before the form opens.
+
