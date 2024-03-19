dbdfbfd3c7ae3f78058661bd47171cf47421f89c
nguyentrananhthu9103@gmail.com <nguyentrananhthu9103@gmail.com>
2024-03-19 08:21:02 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -2,8 +2,21 @@ from ._anvil_designer import Form1Template
 from anvil import *
 
 class Form1(Form1Template):
+  
+
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
+    def insertion_sort(arr):
+      for i in range(1, len(arr)):
+        current_element = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > current_element:
+          arr[j + 1] = arr[j]
+          j -= 1
+        arr[j + 1] = current_element
+      return arr
     self.init_components(**properties)
+    arr = list(map(int, input(self.txtN.split(",")))
 
     # Any code you write here will run before the form opens.
+pass
